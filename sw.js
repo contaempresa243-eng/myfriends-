b6ef24657d363859684e177c14736e0e731a91cc
contaempresa243-eng <contaempresa243@gmail.com>
2026-09-07 09:12:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
+const CACHE_NAME = 'myfriens-v2';
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('myfriens-v1').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(['./', './index.html', './style.css', './app.js', './manifest.json']);
     })
+  );
+});
+
+// Apaga caches de versões antigas para nunca mais ficar preso em ficheiros desatualizados
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((nomes) => {
+      return Promise.all(
+        nomes.filter((nome) => nome !== CACHE_NAME).map((nome) => caches.delete(nome))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
